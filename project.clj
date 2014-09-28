38980222c643b4dff8c55a654f73c0a83b04b7a7
Carsten Behring <carsten.behring@gmail.com>
2014-09-28 11:22:42 +0200

    fixed profile

--- project.clj
+++ project.clj
@@ -5,16 +5,18 @@
                  [compojure "1.1.8"]
                  [digitalocean "1.2"]
 
-                 [org.apache.jclouds/jclouds-all "1.8.0"]
-                 [org.apache.jclouds.driver/jclouds-sshj "1.8.0"]
+            ;     [org.apache.jclouds/jclouds-all "1.8.0"]
+            ;     [org.apache.jclouds.driver/jclouds-sshj "1.8.0"]
                  ]
-  :plugins [[lein-ring "0.8.11"]
+  :plugins [
+;[lein-ring "0.8.11"]
 [environ/environ.lein "0.2.1"]
 ]
-  :ring {:handler ropen-sci-cloud.handler/app}
+ ; :ring {:handler ropen-sci-cloud.handler/app}
   :min-lein-version "2.0.0"
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}
+  {
+;:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+   ;                     [ring-mock "0.1.5"]]}
 :production {:env {:production true}}
                         })
