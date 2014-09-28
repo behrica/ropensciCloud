200d7dfd1f4d3da779aad4c713fc7342f817928f
Carsten Behring <carsten.behring@gmail.com>
2014-09-28 23:35:17 +0200

    disabled version plugin

--- project.clj
+++ project.clj
@@ -14,13 +14,13 @@
             :plugins [
                        [lein-ring "0.8.11"]
                        [environ/environ.lein "0.2.1"]
-                       [org.clojars.cvillecsteele/lein-git-version "1.1.3"]
+                       ;[org.clojars.cvillecsteele/lein-git-version "1.1.3"]
                        ]
             :hooks [environ.leiningen.hooks]
             :uberjar-name "ropen-sci-cloud-standalone.jar"
             :ring {:handler ropen-sci-cloud.web/app}
             :min-lein-version "2.0.0"
-            :middleware [lein-git-version.plugin/middleware]
+            ;:middleware [lein-git-version.plugin/middleware]
             :profiles
             {
               :dev {:dependencies [[ring-mock "0.1.5"]
